fbe16cdc15723fd49f1c08eecbab49cb0fbe9ba7
michael werneburg <michael@werneburg.ca>
2026-05-10 16:43:24 +0900

    Tweak to prevent science advisor pop-ups from nuking the cassette palette.
    The portrait, the for loop that overwrote palette indices 144–255, and the using CivOne.Advances import are all gone. The dialog now sizes itself to fit the text with a minimum width of 140px, uses the Cassette palette untouched from BaseDialog, and places the advisor name and message lines flush to the left edge.

--- src/Screens/Dialogs/AdvisorMessage.cs
+++ src/Screens/Dialogs/AdvisorMessage.cs
@@ -7,9 +7,7 @@
 // You should have received a copy of the CC0 legalcode along with this
 // work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
-using System.Drawing;
 using System.Linq;
-using CivOne.Advances;
 using CivOne.Enums;
 using CivOne.Graphics;
 
@@ -29,32 +27,24 @@ namespace CivOne.Screens.Dialogs
 
 		private static int DialogWidth(string[] message)
 		{
-			int maxWidth = TextBitmaps(message).Max(b => b.Width) + 52;
-			if (maxWidth < 94) maxWidth = 94;
-			return maxWidth;
+			int maxWidth = TextBitmaps(message).Max(b => b.Width) + 16;
+			return maxWidth < 140 ? 140 : maxWidth;
 		}
 
-		public AdvisorMessage(Advisor advisor, string[] message, bool leftAlign) : base((leftAlign ? 38 : 58), 72, DialogWidth(message) + 52, 62)
+		private static int DialogHeight(string[] message)
+		{
+			return 4 + 9 + 4 + TextBitmaps(message).Sum(b => b.Height) + 6;
+		}
+
+		public AdvisorMessage(Advisor advisor, string[] message, bool leftAlign) : base((leftAlign ? 38 : 58), 72, DialogWidth(message), DialogHeight(message))
 		{
 			string[] advisorNames = new string[] { "Defense Minister", "Domestic Advisor", "Foreign Minister", "Science Advisor" };
-			bool modernGovernment = Human.HasAdvance<Invention>();
-			IBitmap governmentPortrait = Icons.GovernmentPortrait(Human.Government, advisor, modernGovernment);
-			
-			Palette palette = Common.DefaultPalette;
-			using (Palette cass = CassetteTheme.CreatePalette())
-				palette.MergePalette(cass, 1, 17);
-			for (int i = 144; i < 256; i++)
-			{
-				palette[i] = governmentPortrait.Palette[i];
-			}
-			this.SetPalette(palette);
-			
+
 			_textLines = TextBitmaps(message);
-			DialogBox.AddLayer(governmentPortrait, 2, 2);
-			DialogBox.DrawText($"{advisorNames[(int)advisor]}:", 0, 15, 47, 4);
-			DialogBox.FillRectangle(47, 11, Resources.GetText($"{advisorNames[(int)advisor]}:", 0, 15).Width + 1, 1, 11);
+			DialogBox.DrawText($"{advisorNames[(int)advisor]}:", 0, 15, 8, 4);
+			DialogBox.FillRectangle(8, 11, Resources.GetText($"{advisorNames[(int)advisor]}:", 0, 15).Width + 1, 1, 11);
 			for (int i = 0; i < _textLines.Length; i++)
-				DialogBox.AddLayer(_textLines[i], 47, (_textLines[i].Height * i) + 13);
+				DialogBox.AddLayer(_textLines[i], 8, (_textLines[i].Height * i) + 13);
 		}
 	}
 }
